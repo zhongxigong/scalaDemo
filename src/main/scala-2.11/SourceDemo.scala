a2e4e1a6539d5e80af3cf1085e350423238dc187
yaoyaoyuan <2703459657@qq.com>
2016-11-02 18:07:05 +0800

    添加文件操作

---
+++ src/main/scala-2.11/SourceDemo.scala
@@ -0,0 +1,22 @@
+import java.io._
+import java.nio.file._
+import scala.io.Source
+
+/**
+  * Created by 耀源 on 2016/11/2.
+  */
+object SourceDemo {
+  def main(args: Array[String]): Unit = {
+    val str = Source.fromFile("c://a.txt").mkString//读取小文件
+    println(str)
+  }
+}
+
+object SourceBigFile {
+  def main(args: Array[String]): Unit = {
+    val iterator = Source.fromFile("c://a.txt").toIterator
+    while(iterator.hasNext){
+      println(iterator.next())
+    }
+  }
+}
